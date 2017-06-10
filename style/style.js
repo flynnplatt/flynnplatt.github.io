d951e622944c971e92e0e8a3e03f59e78b5c1203
Flynn Platt <flynn.platt@jpl.nasa.gov>
2017-06-09 21:27:19 -0700

    actor page done except for mobile

---
+++ style/style.js
@@ -0,0 +1,41 @@
+Shadowbox.init();
+
+function closeReelViewers() {
+    $(".reel-viewer").each(function() {
+        var el = $(this);
+        if (el.hasClass("active")) {
+            el.removeClass("active");
+            setTimeout(function() {
+                el.find("iframe").attr("src", "");
+            }, 500);
+        }
+    });
+}
+
+function createListeners() {
+    $(".reel-trigger").on("click", function(evt) {
+        closeReelViewers();
+        var targetReel = $("#" + $(this).attr("rel"));
+        targetReel.find("iframe").attr("src", targetReel.attr("rel"));
+        targetReel.addClass("active");
+    });
+
+    $(".reel-viewer-close").on("click", function(evt) {
+        closeReelViewers();
+    });
+
+    $(".reel-viewer").on("click", function(evt) {
+        evt.stopPropagation();
+        closeReelViewers();
+    });
+
+    $(document).keyup(function(evt) {
+        if (evt.keyCode == 27) {
+            closeReelViewers();
+        }
+    });
+}
+
+$(document).ready(function() {
+    createListeners();
+});
