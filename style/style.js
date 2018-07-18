f9faa5ad7ffae08c878756669809d0d53c972c43
Flynn Platt <flynn.platt@jpl.nasa.gov>
2018-07-18 14:40:54 -0700

    removed custom image viewer in favor of lightgallery

--- style/style.js
+++ style/style.js
@@ -10,23 +10,6 @@ function closeReelViewers() {
   });
 }
 
-function closeImageViewer() {
-  $(".image-viewer").each(function() {
-    var viewer = $(this);
-    if (viewer.hasClass("active")) {
-      viewer.removeClass("active");
-    }
-    viewer.find("img").each(function() {
-      var img = $(this);
-      img.attr("src", "");
-    });
-    viewer.find(".image-view").each(function() {
-      var view = $(this);
-      view.css("background-image", "none");
-    });
-  });
-}
-
 function createListeners() {
   $(".reel-trigger").on("click", function(evt) {
     closeReelViewers();
@@ -47,60 +30,13 @@ function createListeners() {
   $(document).keyup(function(evt) {
     if (evt.keyCode == 27) {
       closeReelViewers();
-      closeImageViewer();
-    }
-  });
-
-  $(".image-trigger").on("click", function(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    closeImageViewer();
-
-    var trigger = $(this);
-    var src = trigger.attr("href");
-    $(".image-viewer").each(function() {
-      var viewer = $(this);
-
-      viewer.find(".loading").each(function() {
-        var loading = $(this);
-        if (loading.hasClass("hidden")) {
-          loading.removeClass("hidden");
-        }
-      });
-
-      viewer.find("img").each(function() {
-        var img = $(this);
-        img.load(function(loadEvt) {
-          viewer.find(".image-view").each(function() {
-            var view = $(this);
-            view.css("background-image", "url(" + src + ")");
-          });
-          viewer.find(".loading").each(function() {
-            var loading = $(this);
-            if (!loading.hasClass("hidden")) {
-              loading.addClass("hidden");
-            }
-          });
-        });
-        img.attr("src", src);
-      });
-      if (!viewer.hasClass("active")) {
-        viewer.addClass("active");
-      }
-    });
-    $(".image-viewer .close-btn").on("click", function(evt) {
-      closeImageViewer();
-    });
-  });
-
-  $(document).click(function(event) {
-    if (!$(event.target).closest(".image-viewer").length) {
-      $(".image-viewer").removeClass("active");
     }
   });
 }
 
 $(document).ready(function() {
   createListeners();
+  lightGallery(document.getElementById("lightgallery"), {
+    mode: "lg-fade"
+  });
 });
